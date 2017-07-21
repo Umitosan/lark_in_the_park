7a17fffcdf6bdb7e0f412892bd095142c2847f2b
Dominic Brown <yeahdom@gmail.com>
2017-07-21 12:55:05 -0700

    add setup for rspec, add some basic specs for STATE

---
+++ spec/factories/parks.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :park do
+    name('Jurassic Park')
+    description('the best place on earth')
+    national_park(false)
+  end
+end
---
+++ spec/requests/get_stats_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "get all states route", :type => :request do
+  let!(:states) { FactoryGirl.create_list(:state, 10)}
+
+  before { get '/states'}
+
+  it 'returns all states' do
+    expect(JSON.parse(response.body).size).to eq(10)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+  
+end
